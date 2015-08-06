bab5f9b5b2be036534ae43db468e6ad1a36fe068
Max Lv <mlv@nvidia.com>
2015-08-06 10:09:27 +0800

    support port_password json conf

--- src/jconf.h
+++ src/jconf.h
@@ -22,8 +22,9 @@
 #ifndef _JCONF_H
 #define _JCONF_H
 
+#define MAX_PORT_NUM 1024
 #define MAX_REMOTE_NUM 10
-#define MAX_CONF_SIZE 16 * 1024
+#define MAX_CONF_SIZE 128 * 1024
 #define MAX_DNS_NUM 4
 #define MAX_CONNECT_TIMEOUT 10
 #define MIN_UDP_TIMEOUT 60
@@ -34,8 +35,15 @@ typedef struct {
 } ss_addr_t;
 
 typedef struct {
+    char *port;
+    char *password;
+} ss_port_password_t;
+
+typedef struct {
     int remote_num;
     ss_addr_t remote_addr[MAX_REMOTE_NUM];
+    int port_password_num;
+    ss_port_password_t port_password[MAX_PORT_NUM];
     char *remote_port;
     char *local_addr;
     char *local_port;
